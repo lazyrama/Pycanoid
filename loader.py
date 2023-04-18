65f4cbd09483c0f8061593b6041eddf2d3679cb4
abobus <vasram2706@gmail.com>
2023-04-18 15:45:03 +0500

    Add sources

--- loader.py
+++ loader.py
@@ -1,37 +1,4 @@
-from settings import VIRTUAL_PIXEL
-from entities import Block
-
-
 class LevelLoader:
-    def __init__(self):
-        levels = []
-
-        with open("levels.txt", "r") as f:
-            flag = False
-            level = []
-            lines = f.readlines()
-            for line in lines:
-                if "\"\"\"" in line and not flag:
-                    flag = True
-                elif "\"\"\"" in line and flag:
-                    levels.append(level)
-                    level = []
-                    flag = False
-                else:
-                    level.append(line.rstrip('\n'))
-        self.campaign = []
-        for i, level in enumerate(levels):
-            level1 = []
-            for j, line in enumerate(level):
-                line1 = []
-                for k, block in enumerate(line):
-                    if block == "#":
-                        line1.append(Block(k * 4 * VIRTUAL_PIXEL, 2 * VIRTUAL_PIXEL + j * VIRTUAL_PIXEL,
-                                           ))
-                level1.append(line1)
-            self.campaign.append(level1)
-
-    def draw(self, screen, level):
-        for line in self.campaign[level]:
-            for block in line:
-                block.draw(screen)
+	def __init__():
+		self.blocks = []
+		pass
--- settings.py
+++ settings.py
@@ -1,14 +1,4 @@
-from enum import Enum
-
-import pygame
-
-WIDTH, HEIGHT = 1080, 768
+WIDTH, HEIGHT = 0, 0
 SCORE_PER_BLOCK = 100
 LOAD_CUSTOM = False
-VIRTUAL_PIXEL = 18
-
-
-class GameState(Enum):
-    MENU = 0
-    LEVEL = 1
-    GAME_OVER = 2
+VIRTUAL_PIXEL = 10
