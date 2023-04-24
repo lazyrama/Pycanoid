194e4ef224cc18acb8cedecb7ab16b6812f85040
lazyrama <vasram2706@gmail.com>
2023-04-24 15:42:12 +0500

    Some another

--- entities.py
+++ entities.py
@@ -1,29 +1,40 @@
+import pygame
+from settings import VIRTUAL_PIXEL
+from settings import WIDTH, HEIGHT
 class Racket:
-	def __init__(): 
+	def __init__(self): 
 		self.x = WIDTH//2 - 3 * VIRTUAL_PIXEL
 		self.y = HEIGHT - VIRTUAL_PIXEL
+		self.rect = pygame.Rect(self.x, self.y,  6*VIRTUAL_PIXEL, VIRTUAL_PIXEL)
 		pass
-	def update():
+	def update(self):
 		pass
-	def draw():
+	def draw(self,screen):
+		pygame.draw.rect(screen, (0, 255, 0), self.rect)
 		pass
 		
-class Block():
-	def __init__(x, y):
+class Block:
+	def __init__(self, x, y):
 		self.x = x
 		self.y = y
 		self.is_destroyed = False
-	def update():
+	def update(self):
 		pass
-	def draw():
+	def draw(self, screen):
+		pygame.draw.rect()
 		pass
 		
 class Ball:
-	def __init__():
+	def __init__(self):
 		self.x = WIDTH//2 - VIRTUAL_PIXEL//2
 		self.y = HEIGHT - 2 * VIRTUAL_PIXEL
+		self.x_speed = 1
+		self.y_speed = -1
+		self.coeff = 5
+		self.rect = pygame.Rect(self.x, self.y, VIRTUAL_PIXEL, VIRTUAL_PIXEL)
 		pass
-	def update():
+	def update(self):
 		pass
-	def draw():
+	def draw(self, screen):
+		pygame.draw.rect(screen, (255, 0, 0), self.rect)
 		pass
--- main.py
+++ main.py
@@ -1,10 +1,24 @@
+import pygame
+from entities import Racket, Ball
+from settings import WIDTH, HEIGHT
 class Arcanoid:
-	def __init__():
+	def __init__(self):
+		pygame.init()
+		self.screen = pygame.display.set_mode((WIDTH, HEIGHT), flags=pygame.SCALED)
+		self.racket = Racket()
+		self.ball = Ball()
+		self.running = True
 		pass
-	def update():
+	def update(self):
 		pass
-	def draw():
-		pass
-	def run():
-		pass
- 	
+	def draw(self):
+		self.screen.fill((0, 0, 0))
+		self.racket.draw(self.screen)
+		self.ball.draw(self.screen)
+		pygame.display.update()
+	def run(self):
+		while self.running:
+			self.draw()
+
+game = Arcanoid()
+game.run()
