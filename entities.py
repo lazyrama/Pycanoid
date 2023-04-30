b60ddcc705bc7b2e9141e20581e270e67fec36d7
lazyrama <vasram2706@gmail.com>
2023-04-30 13:51:19 +0500

    Added collision with wall and racket, racket control

--- entities.py
+++ entities.py
@@ -1,40 +1,57 @@
 import pygame
 from settings import VIRTUAL_PIXEL
 from settings import WIDTH, HEIGHT
+
+
 class Racket:
-	def __init__(self): 
-		self.x = WIDTH//2 - 3 * VIRTUAL_PIXEL
-		self.y = HEIGHT - VIRTUAL_PIXEL
-		self.rect = pygame.Rect(self.x, self.y,  6*VIRTUAL_PIXEL, VIRTUAL_PIXEL)
-		pass
-	def update(self):
-		pass
-	def draw(self,screen):
-		pygame.draw.rect(screen, (0, 255, 0), self.rect)
-		pass
-		
+    def __init__(self):
+        self.x = WIDTH // 2 - 3 * VIRTUAL_PIXEL
+        self.y = HEIGHT - VIRTUAL_PIXEL
+        self.rect = pygame.Rect(self.x, self.y, 6 * VIRTUAL_PIXEL, VIRTUAL_PIXEL)
+        pass
+
+    def update(self):
+        self.rect.x = self.x
+        self.rect.y = self.y
+        pass
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, (0, 255, 0), self.rect)
+        pass
+
+
 class Block:
-	def __init__(self, x, y):
-		self.x = x
-		self.y = y
-		self.is_destroyed = False
-	def update(self):
-		pass
-	def draw(self, screen):
-		pygame.draw.rect()
-		pass
-		
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.is_destroyed = False
+
+    def update(self):
+        pass
+
+    def draw(self, screen):
+        pygame.draw.rect()
+        pass
+
+
 class Ball:
-	def __init__(self):
-		self.x = WIDTH//2 - VIRTUAL_PIXEL//2
-		self.y = HEIGHT - 2 * VIRTUAL_PIXEL
-		self.x_speed = 1
-		self.y_speed = -1
-		self.coeff = 5
-		self.rect = pygame.Rect(self.x, self.y, VIRTUAL_PIXEL, VIRTUAL_PIXEL)
-		pass
-	def update(self):
-		pass
-	def draw(self, screen):
-		pygame.draw.rect(screen, (255, 0, 0), self.rect)
-		pass
+    def __init__(self):
+        self.x = WIDTH // 2 - VIRTUAL_PIXEL // 2
+        self.y = HEIGHT - 2 * VIRTUAL_PIXEL
+        self.x_speed = 0.35
+        self.y_speed = -0.35
+        self.coeff = 5
+        self.rect = pygame.Rect(self.x, self.y, VIRTUAL_PIXEL, VIRTUAL_PIXEL)
+        pass
+
+    def update(self, delta):
+        self.rect.x += self.x_speed * delta
+        self.rect.y += self.y_speed * delta
+        if self.rect.x >= (WIDTH - VIRTUAL_PIXEL) or self.rect.x <= VIRTUAL_PIXEL:
+            self.x_speed *= -1
+        if self.rect.y >= (HEIGHT - VIRTUAL_PIXEL) or self.rect.y <= VIRTUAL_PIXEL:
+            self.y_speed *= -1
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, (255, 0, 0), self.rect)
+        pass
--- main.py
+++ main.py
@@ -1,24 +1,52 @@
 import pygame
+from pygame.time import Clock as clock
 from entities import Racket, Ball
 from settings import WIDTH, HEIGHT
+
+
 class Arcanoid:
 	def __init__(self):
-		pygame.init()
-		self.screen = pygame.display.set_mode((WIDTH, HEIGHT), flags=pygame.SCALED)
+		pygame.init() #
+		self.screen = pygame.display.set_mode((WIDTH, HEIGHT), flags=pygame.FULLSCREEN)
 		self.racket = Racket()
 		self.ball = Ball()
 		self.running = True
+		self.clock = clock()
 		pass
+
 	def update(self):
-		pass
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_ESCAPE:
+					pygame.quit()
+
+		key = pygame.key.get_pressed()
+		if key[pygame.K_a] or key[pygame.K_LEFT]:
+			self.racket.rect.left -= 1 * self.clock.get_time()
+		if key[pygame.K_d] or key[pygame.K_RIGHT]:
+			self.racket.rect.right += 1 * self.clock.get_time()
+
+		if self.racket.rect.colliderect(self.ball.rect):
+			self.ball.y_speed *= -1
+			self.ball.x_speed *= -1
+
+		#self.racket.update()
+		self.ball.update(self.clock.get_time())
+
 	def draw(self):
 		self.screen.fill((0, 0, 0))
 		self.racket.draw(self.screen)
 		self.ball.draw(self.screen)
 		pygame.display.update()
+
 	def run(self):
 		while self.running:
+			self.update()
 			self.draw()
+			self.clock.tick(30.0)
+
 
 game = Arcanoid()
 game.run()
