75d5b479c8b2ba691ec29a4c1775c104c07f0dc6
lazyrama <vasram2706@gmail.com>
2023-04-30 16:34:54 +0500

    Made simple system for loading and drawing levels

--- loader.py
+++ loader.py
@@ -1,5 +1,39 @@
 import pygame
+from settings import VIRTUAL_PIXEL
+
+
 class LevelLoader:
-	def __init__():
-		self.blocks = []
-		pass
+    def __init__(self):
+        levels = []
+
+        with open("levels.txt", "r") as f:
+            flag = False
+            level = []
+            lines = f.readlines()
+            for line in lines:
+                if "\"\"\"" in line and not flag:
+                    flag = True
+                elif "\"\"\"" in line and flag:
+                    levels.append(level)
+                    level = []
+                    flag = False
+                else:
+                    level.append(line.rstrip('\n'))
+        self.campaign = []
+        for i, level in enumerate(levels):
+            level1 = []
+            for j, line in enumerate(level):
+                line1 = []
+                for k, block in enumerate(line):
+                    if block == "#":
+                        print(543, block)
+                        line1.append((k * 6 * VIRTUAL_PIXEL, j * 2 * VIRTUAL_PIXEL,
+                                          VIRTUAL_PIXEL * 4, VIRTUAL_PIXEL))
+                level1.append(line1)
+            self.campaign.append(level1)
+
+    def draw(self, screen):
+        for line in self.campaign[0]:
+            for block in line:
+                print(block)
+                pygame.draw.rect(screen, (0, 255, 255), block)
