140f2c1036752b82052be9d4dc9b2433cef43f4e
lazyrama <vasram2706@gmail.com>
2023-05-13 10:36:15 +0500

    added very simple menu

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import pygame
 from pygame.time import Clock
 from entities import Racket, Ball
-from settings import WIDTH, HEIGHT, VIRTUAL_PIXEL
+from settings import WIDTH, HEIGHT, VIRTUAL_PIXEL, GameState
 from loader import LevelLoader
+from menu import Menu
+
+pygame.init()
+font = pygame.font.Font('freesansbold.ttf', 32)
 
 
 class Arcanoid:
     def __init__(self):
-        pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT), flags=pygame.FULLSCREEN | pygame.SCALED)
         self.alpha_surface = pygame.Surface((WIDTH, HEIGHT))
         self.alpha_surface.set_alpha(40)
+        self.menu = Menu(font)
 
         self.racket = Racket()
         self.ball = Ball()
@@ -18,17 +22,16 @@ class Arcanoid:
         self.level_num = 0
         self.clock = Clock()
 
-        self.font = pygame.font.Font('freesansbold.ttf', 32)
         self.score = 0
-        self.scoreFont = self.font.render('{}'.format(self.score), True, (0, 255, 0), (0, 0, 255))
+        self.scoreFont = font.render('{}'.format(self.score), True, (0, 255, 0), (0, 0, 255))
         self.scoreRect = self.scoreFont.get_rect()
 
-        self.text = self.font.render('Game Over', True, (0, 255, 0), (0, 0, 255))
+        self.text = font.render('Game Over', True, (0, 255, 0), (0, 0, 255))
         self.textRect = self.text.get_rect()
         # set the center of the rectangular object.
         self.textRect.center = (WIDTH // 2, HEIGHT // 2)
 
-        self.running = True
+        self.state = GameState.MENU
 
     def is_level_ended(self):
         for line in self.loader.campaign[self.level_num]:
@@ -44,90 +47,90 @@ class Arcanoid:
                 pygame.quit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-                    self.running = False
                     pygame.display.quit()
                     pygame.quit()
                     quit()
+            if event.type == pygame.MOUSEBUTTONUP and self.state == GameState.MENU:
+                if self.menu.update(pygame.mouse.get_pos()):
+                    self.state = GameState.LEVEL
 
-        if self.is_level_ended():
-            self.score = 0
-            self.level_num += 1
-            self.racket.x, self.racket.y = WIDTH // 2 - 3 * VIRTUAL_PIXEL, HEIGHT - VIRTUAL_PIXEL
-            self.ball.x, self.ball.y = WIDTH // 2 - VIRTUAL_PIXEL // 2, HEIGHT - 2 * VIRTUAL_PIXEL
+        if self.state == GameState.LEVEL:
+            if self.is_level_ended():
+                self.score = 0
+                self.level_num += 1
+                self.racket.x, self.racket.y = WIDTH // 2 - 3 * VIRTUAL_PIXEL, HEIGHT - VIRTUAL_PIXEL
+                self.ball.x, self.ball.y = WIDTH // 2 - VIRTUAL_PIXEL // 2, HEIGHT - 2 * VIRTUAL_PIXEL
 
-        key = pygame.key.get_pressed()
-        if key[pygame.K_a] or key[pygame.K_LEFT]:
-            self.racket.x -= 1 * self.clock.get_time()
-        if key[pygame.K_d] or key[pygame.K_RIGHT]:
-            self.racket.x += 1 * self.clock.get_time()
-        self.racket.update()
+            key = pygame.key.get_pressed()
+            if key[pygame.K_a] or key[pygame.K_LEFT]:
+                self.racket.x -= 1 * self.clock.get_time()
+            if key[pygame.K_d] or key[pygame.K_RIGHT]:
+                self.racket.x += 1 * self.clock.get_time()
+            self.racket.update()
 
-        if self.racket.colliderect(self.ball):
-            if self.ball.x > (self.racket.centerx + VIRTUAL_PIXEL // 2) and self.ball.x_speed < 0:
-                self.ball.y_speed *= -1
-                self.ball.x_speed *= -1
-                if self.ball.x_speed < 0:
-                    self.ball.x_speed -= 0.01
-                if self.ball.x_speed > 0:
-                    self.ball.x_speed += 0.01
+            if self.racket.colliderect(self.ball):
+                if self.ball.x > (self.racket.centerx + VIRTUAL_PIXEL // 2) and self.ball.x_speed < 0:
+                    self.ball.y_speed *= -1
+                    self.ball.x_speed *= -1
+                    if self.ball.x_speed < 0:
+                        self.ball.x_speed -= 0.01
+                    if self.ball.x_speed > 0:
+                        self.ball.x_speed += 0.01
 
-            elif self.ball.x < (self.racket.centerx - VIRTUAL_PIXEL // 2) and self.ball.x_speed > 0:
-                self.ball.y_speed *= -1
-                self.ball.x_speed *= -1
-                if self.ball.x_speed < 0:
-                    self.ball.x_speed -= 0.01
-                if self.ball.x_speed > 0:
-                    self.ball.x_speed += 0.01
+                elif self.ball.x < (self.racket.centerx - VIRTUAL_PIXEL // 2) and self.ball.x_speed > 0:
+                    self.ball.y_speed *= -1
+                    self.ball.x_speed *= -1
+                    if self.ball.x_speed < 0:
+                        self.ball.x_speed -= 0.01
+                    if self.ball.x_speed > 0:
+                        self.ball.x_speed += 0.01
+                else:
+                    self.ball.y_speed *= -1
             else:
-                self.ball.y_speed *= -1
-        else:
-            if self.ball.x_speed > 0 and self.ball.x_speed > 1:
-                self.ball.x_speed -= 0.01
-            elif self.ball.x_speed < 0 and self.ball.x_speed < -1:
-                self.ball.x_speed += 0.01
+                if self.ball.x_speed > 0 and self.ball.x_speed > 1:
+                    self.ball.x_speed -= 0.01
+                elif self.ball.x_speed < 0 and self.ball.x_speed < -1:
+                    self.ball.x_speed += 0.01
 
-        is_game_over = self.ball.update(self.clock.get_time())
-        if is_game_over:
-            self.running = False
-        for line in self.loader.campaign[self.level_num]:
-            for block in line:
-                if self.ball.colliderect(block) and not block.is_destroyed:
-                    block.is_destroyed = True
-                    self.score += 1
+            is_game_over = self.ball.update(self.clock.get_time())
+            if is_game_over:
+                self.state = GameState.GAME_OVER
+            for line in self.loader.campaign[self.level_num]:
+                for block in line:
+                    if self.ball.colliderect(block) and not block.is_destroyed:
+                        block.is_destroyed = True
+                        self.score += 1
 
-        self.scoreFont = self.font.render('{}'.format(self.score), True, (0, 255, 0), (0, 0, 255))
-        self.scoreRect = self.scoreFont.get_rect()
+            self.scoreFont = font.render('{}'.format(self.score), True, (0, 255, 0), (0, 0, 255))
+            self.scoreRect = self.scoreFont.get_rect()
+        elif self.state == GameState.GAME_OVER:
+            print(2)
 
     def draw(self):
         # self.screen.fill((0, 0, 0))
-        self.screen.blit(self.alpha_surface, (0, 0))
-        self.screen.blit(self.scoreFont, self.scoreRect)
+        if self.state == GameState.MENU:
+            self.menu.draw(self.screen)
 
-        self.racket.draw(self.screen)
-        self.ball.draw(self.screen)
-        self.loader.draw(self.screen, self.level_num)
+        if self.state == GameState.LEVEL:
+            self.screen.blit(self.alpha_surface, (0, 0))
+            self.screen.blit(self.scoreFont, self.scoreRect)
+
+            self.racket.draw(self.screen)
+            self.ball.draw(self.screen)
+            self.loader.draw(self.screen, self.level_num)
+
+        if self.state == GameState.GAME_OVER:
+            self.screen.fill((0, 0, 0))
+            self.screen.blit(self.text, self.textRect)
 
         pygame.display.update()
 
     def run(self):
         print(self.is_level_ended())
-        while self.running:
+        while True:
             self.update()
             self.draw()
             self.clock.tick(30.0)
-
-        self.screen.fill((0, 0, 0))
-        self.screen.blit(self.text, self.textRect)
-        pygame.display.update()
-
-        while not self.running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                    pygame.display.quit()
-                    pygame.quit()
-                    quit()
 
 
 game = Arcanoid()
